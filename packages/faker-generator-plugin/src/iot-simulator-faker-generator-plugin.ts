0aa93aa84b2cd1c09fd91a052442cbaa94ebb707
Ivan Greguric Ortolan <ivan.g.ortolan@gmail.com>
2020-01-09 18:00:54 +0100

    Small refactor on faker

--- packages/faker-generator-plugin/src/iot-simulator-faker-generator-plugin.ts
+++ packages/faker-generator-plugin/src/iot-simulator-faker-generator-plugin.ts
@@ -1,11 +1,10 @@
-import * as Faker from 'faker'
+import * as faker from 'faker'
 import { StatelessGeneratorPlugin, STATELESS_TYPE, provideNamed } from 'iot-simulator-shared'
 
 @provideNamed(STATELESS_TYPE, 'faker')
 export default class StatelessFakerGeneratorPlugin
   implements StatelessGeneratorPlugin<string, string> {
-
   generate(config: string): string {
-    return Faker.fake(config)
+    return faker.fake(config)
   }
 }
