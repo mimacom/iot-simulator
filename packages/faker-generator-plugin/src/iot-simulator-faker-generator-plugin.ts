8370a35c6cdd7b2b46d78ede4aa164c89f6b4b59
Ivan Greguric Ortolan <ivan.g.ortolan@gmail.com>
2020-01-09 15:00:51 +0100

    fix plugin to use new decorator

--- packages/faker-generator-plugin/src/iot-simulator-faker-generator-plugin.ts
+++ packages/faker-generator-plugin/src/iot-simulator-faker-generator-plugin.ts
@@ -1,15 +1,11 @@
-// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-  // import "core-js/fn/array.find"
-  // ...
-import {injectable} from "inversify";
-import * as Faker from "faker";
-import "reflect-metadata";
-import {StatelessGeneratorPlugin} from "iot-simulator-shared";
+import * as Faker from 'faker'
+import { StatelessGeneratorPlugin, STATELESS_TYPE, provideNamed } from 'iot-simulator-shared'
 
-@injectable()
-export default class StatelessFakerGeneratorPlugin implements StatelessGeneratorPlugin<string, string> {
+@provideNamed(STATELESS_TYPE, 'faker')
+export default class StatelessFakerGeneratorPlugin
+  implements StatelessGeneratorPlugin<string, string> {
 
-  generate(config: any): any {
-    return Faker.fake(config);
+  generate(config: string): string {
+    return Faker.fake(config)
   }
 }
--- packages/faker-generator-plugin/test/iot-simulator-faker-generator-plugin.test.ts
+++ packages/faker-generator-plugin/test/iot-simulator-faker-generator-plugin.test.ts
@@ -1,7 +1,7 @@
+import 'reflect-metadata'
 import StatelessFakerGeneratorPlugin from "../src/iot-simulator-faker-generator-plugin"
 
 describe("StatelessFakerGeneratorPlugin test", () => {
-
 
   it("Generate name by string", () => {
     expect(new StatelessFakerGeneratorPlugin().generate("{{name.findName}}")).toEqual(expect.any(String));
